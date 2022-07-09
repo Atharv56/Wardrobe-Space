68a1d520d1451a0b72c477b9e4621d0aef9a18ec
Atharv56 <69430707+Atharv56@users.noreply.github.com>
2022-07-09 22:14:55 +0530

    initial

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    clrscr();
+    int x;
+    printf("Welcome to WardrobeSpace!!!");
+    printf("\nLets pick an outfit for you");
+    printf("\nPlease enter your gender: ");
+    scanf("%d", &x);
+    switch(x)
+}
