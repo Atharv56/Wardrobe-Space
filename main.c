20b383f06260b58083f51321ff395a8090545f7c
Atharv56 <69430707+Atharv56@users.noreply.github.com>
2022-07-10 08:06:39 +0530

    first display page done

--- main.c
+++ main.c
@@ -1,11 +1,24 @@
 #include<stdio.h>
 #include<conio.h>
 int main(){
-    clrscr();
     int x;
     printf("Welcome to WardrobeSpace!!!");
     printf("\nLets pick an outfit for you");
     printf("\nPlease enter your gender: ");
+    printf("\n");
     scanf("%d", &x);
     switch(x)
+    {
+        case 1: 
+        //male();
+        printf("Male");
+        break;
+        case 2:
+        printf("Female");
+        break;
+        //female();
+        default:
+            printf("Gender not specified");
+    }
+    return 0;
 }
