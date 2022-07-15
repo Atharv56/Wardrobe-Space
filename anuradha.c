b7d0e90c60aaa63d19aa75badd88374243a128e1
anuradha-yeole <anuradhasudhiryeole@gmail.com>
2022-07-15 13:50:10 +0530

    Update anuradha.c

--- anuradha.c
+++ anuradha.c
@@ -1,64 +1,110 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
+/
 #include <stdio.h>
 #include<conio.h>
 
-int main()
+int datenight()
 {
-    int i,j,n;
-    char outfit1[][8000] = {"blazer+meshed top+leatherpants+wedges+gold jewellery to look sexier!","mini dress+stiletto+hoop earings","halterneck+skirt+boots+silver jewellery"};
-    float cost1[80]={4000.00,2000.00,1500.00};
-    char outfit2[][8000]={"a simple black dress+sneakers+studded earings","leather pants+tanktop+blackdenims+gold hoops","body suit+cargo pants+silver jwellery"};
-    float cost2[80]={2500.00,2000.00,1560.00};
-    char outfit3[][8000]={"bodysuit+denimjacket+leather pants+pearl jewellery","off shoulder+cargo pants+sneakers","turtleneck+bootcut jeans+whiteshoes+hoop earing"};
-    float cost3[80]={3500.00,1550.00,2890.00};
-    char outfit4[][8000]={"tank top+shorts+sneakers","crop top+mom jeans+flats","cargo pants+tanktop+boots"};
-    float cost4[80]={1545.00,2000.00,1789.00};
-    printf("lets decide your outfit based on time of the day and your current mood\n1.night and glamup\n2.night and simple and elegant\n3.day and glamup\n4.day and simple and elegant");
+    int i,j,k;
+    float sum=0.0;
+    char top1[][100]={"mesh top","halterneck","bodysuit"};
+    float costtop1[100]={1000.00,869.90,990.00};
+    char bottom1[][100]={"leather pants","skirt","cargo pants"};
+    float costbottom1[100]={1080.00,789.90,557.89};
+    char footwear1[][100]={"stilletos","wedges","sneakers"};
+    float costfootwear1[100]={989.89,1000.00,750.00};
+    char top2[][100]={"body suit","off shoulder","tank top"};
+    float costtop2[100]={1000.00,869.90,990.00};
+    char bottom2[][100]={"leather pants","blue denims","cargo pants"};
+    float costbottom2[100]={1080.00,789.90,557.89};
+    char footwear2[][100]={"white sneakers","wedges","boots"};
+    float costfootwear2[100]={989.89,1000.00,750.00};
+    char top3[][100]={"tank top","","bodysuit"};
+    float costtop3[100]={1000.00,869.90,990.00};
+    char bottom3[][100]={"leather pants","skirt","cargo pants"};
+    float costbottom3[100]={1080.00,789.90,557.89};
+    char footwear3[][100]={"stilletos","wedges","sneakers"};
+    float costfootwear3[100]={989.89,1000.00,750.00};
+    printf("choose an option based on time of the day and mood\n1.night and glamedup\n2.night and simple&elegant\n
+    3.day and glamed up\n4.day and simple&elegant");
     scanf("%d",&n);
     switch(n)
     {
         case 1:
-        for(i=0;i<=2;i++)
+            for(i=0;i<=2;i++)
         {
-            printf("here you go\n%s(estimated cost Rs %.2f)\n",outfit1[i],cost1[i]);
+            for(j=0;j<=2;j++)
+            {
+                for(k=0;k<=2;k++)
+                {
+                    printf("outfits:%s+%s+%s\n",top1[i],bottom1[j],footwear1[k]);
+                    sum=sum+costtop1[i]+costbottom1[j]+costfootwear1[k];
+                    printf("estimated price:%f\n",sum);
+                }
+            }
+            
         }
-        break;
-        case 2:
-        for(i=0;i<=2;i++)
+         break;
+         case 2:
+          for(i=0;i<=2;i++)
         {
-            printf("here you go\n%s(estimated cost Rs %.2f)\n",outfit2[i],cost2[i]);
+            for(j=0;j<=2;j++)
+            {
+                for(k=0;k<=2;k++)
+                {
+                    printf("outfits:%s+%s+%s\n",top2[i],bottom2[j],footwear2[k]);
+                    sum=sum+costtop2[i]+costbottom2[j]+costfootwear2[k];
+                    printf("estimated price:%f\n",sum);
+                }
+            }
+            
         }
-        break;
-        case 3:
-        for(i=0;i<=2;i++)
+         break;
+         case 3:
+          for(i=0;i<=2;i++)
         {
-            printf("here you go\n%s(estimated cost Rs %.2f)\n",outfit3[i],cost3[i]);
+            for(j=0;j<=2;j++)
+            {
+                for(k=0;k<=2;k++)
+                {
+                    printf("outfits:%s+%s+%s\n",top3[i],bottom3[j],footwear3[k]);
+                    sum=sum+costtop3[i]+costbottom3[j]+costfootwear3[k];
+                    printf("estimated price:%f\n",sum);
+                }
+            }
+            
         }
         break;
         case 4:
-        for(i=0;i<=2;i++)
+         for(i=0;i<=2;i++)
         {
-            printf("here you go\n%s(estimated cost Rs %.2f)\n",outfit4[i],cost4[i]);
+            for(j=0;j<=2;j++)
+            {
+                for(k=0;k<=2;k++)
+                {
+                    printf("outfits:%s+%s+%s\n",top4[i],bottom4[j],footwear4[k]);
+                    sum=sum+costtop4[i]+costbottom4[j]+costfootwear4[k];
+                    printf("estimated price:%f\n",sum);
+                }
+            }
+            
         }
         break;
         default:
-        printf("oops!option not available");
+        printf("ivalid input");
         break;
-
-    }
          
+    
+    )
+    
+    
+   
+    
     getch();
-    return 0;   
-    }
+    return 0;
+            
         
     
     
 
 
+}
